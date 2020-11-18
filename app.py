6419ecb66cdb59982e0fa53404bc8953efc5abdf
Zachary <zach.gibbs@gmail.com>
2020-11-18 14:57:17 -0500

    Getting there!

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-#Import Flask
-from flask import Flask
+#Import Flask, SqlAlchemy, Numpy
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+
+########################
+#Database Time
+########################
+
+#Engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+#Reflect the Existing Database
+Base = automap_base()
+
+#Reflect The Tables
+Base.prepare(engine, reflect = True)
+
+#Save References
+Stations = Base.classes.station
+Measurements = Base.classes.measurement
+
 
 #Create the App
 app=Flask(__name__)
@@ -9,19 +32,33 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return """Welcome to my homework. There are a few routes you can take here! 
-    Precipitation: /api/v1.0/precipitation
-    Stations: /api/v1.0/stations
-    Temperature: /api/v1.0/tobs
-    Starting Point Data: /api/v1.0/<start>
-    A Range of Dates: /api/v1.0/<start>/<end>"""
-
+    """List the Routes"""
+    return (
+        f"Welcome to my homework. There are a few routes you can take here!"
+        f"Precipitation: /api/v1.0/precipitation"
+        f"Stations: /api/v1.0/stations"
+        f"Temperature: /api/v1.0/tobs"
+        f"Starting Point Data: /api/v1.0/<start>"
+        f"A Range of Dates: /api/v1.0/<start>/<end>"
+    )
 
 #@app.route("/api/v1.0/precipitation")
 #def precipitation():
 
-#@app.route("/api/v1.0/stations")
-#def stations()
+@app.route("/api/v1.0/stations")
+def stations():
+    #Create The Session
+    session = Session(engine)
+
+    #Query the Stations
+    results = session.query(Stations.station, Stations.name, Stations.latitude, Stations.longitude, Stations.elevation)
+
+    session.close()
+
+    #Convert!
+    All_stations = list(np.ravel(results))
+
+    return jsonify(All_stations)
 
 #@app.route("/api/v1.0/tobs")
 #def temperature()
